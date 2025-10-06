0d66eaefd68ec636e33d6c107c3a6b9b1243e8a0
Vinothini87 <vinothini.m1@gmail.com>
2025-10-06 14:26:03 +0530

    Updated server code and created README.md

--- Exp1a_StopWait/server.py
+++ Exp1a_StopWait/server.py
@@ -5,6 +5,11 @@ s.listen(5)
 c,addr=s.accept()
 while True:
     i=input("Enter a data: ")
+    if i.lower() == "exit":       # <-- added exit condition
+        c.send(i.encode())        # send 'exit' so client also stops
+        print("[SERVER] Closing connection...")
+        c.close()
+        break
     c.send(i.encode())
     ack=c.recv(1024).decode()
     if ack:
